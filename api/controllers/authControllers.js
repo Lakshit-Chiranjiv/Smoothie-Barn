defff009b9ede06aa6eecb40357394e1d056d09d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-03 18:57:10 +0530

    created a handle errors func for good err msg

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -1,4 +1,33 @@
+import UserModel from "../models/userModel.js"
 
+const handleErrors = (error) => {
+    let errors = {
+        email: '',
+        username: '',
+        password: ''
+    }
+
+    const { message,code } = error
+
+    if(code === 11000){
+        if(error.keyPattern.email){
+            errors.email = 'Already registered email'
+            return errors
+        }
+        if(error.keyPattern.username){
+            errors.username = 'Already registered username'
+            return errors
+        }
+    }
+
+    if(message.includes('user validation failed')){
+        Object.values(error.errors).forEach(({properties})=>{
+            errors[properties.path] = properties.message
+        })
+    }
+
+    return errors
+}
 
 export const getSignupPage = (req,res) => {
 
@@ -8,8 +37,18 @@ export const getLoginPage = (req,res) => {
 
 }
 
-export const signupUser = (req,res) => {
+export const signupUser = async(req,res) => {
+    const { email,username,password } = req.body
 
+    try {
+        const user = await UserModel.create({
+            email,username,password
+        })
+        res.status(201).json(user)
+    } catch (error) {
+        const errorsObj = handleErrors(error)
+        res.status(400).json(errorsObj)
+    }
 }
 
 export const loginUser = (req,res) => {
