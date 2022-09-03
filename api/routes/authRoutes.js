060472d7835856b85754f7e5106507990900eca1
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-03 17:15:00 +0530

    wired auth contollers to routes

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,20 +1,21 @@
 import express from 'express'
+import { getSignupPage,getLoginPage,signupUser,loginUser,logoutUser } from './../controllers/authControllers.js'
 
 const router = express.Router()
 
 //sign up page
-router.get('/signup',()=>{})
+router.get('/signup',getSignupPage)
 
 //login page
-router.get('/login',()=>{})
+router.get('/login',getLoginPage)
 
 //sign up new user
-router.post('/signup',()=>{})
+router.post('/signup',signupUser)
 
 //login existing user
-router.post('/login',()=>{})
+router.post('/login',loginUser)
 
 //log out existing user
-router.get('/logout',()=>{})
+router.get('/logout',logoutUser)
 
 export default router
