a540e812ebbcfdd64e681447e96877179c73f394
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-03 19:05:34 +0530

    mongoose post hook

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -28,6 +28,13 @@ userSchema.pre('save',function(next){
     //this keyword gives us the access to the req.body before it is saved to the db, as we will use it in pre hook and we can't use arrow func as they don't give us the pre hook
     console.log('user to be created')
     console.log(this)
+    next()
+})
+
+userSchema.post('save',(doc,next)=> {
+    console.log('user created')
+    console.log(doc),
+    next()
 })
 
 const UserModel = mongoose.model('user',userSchema)
