786499fe137e2abc80c21b3b10e9a4b02468c442
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-03 17:10:58 +0530

    smoothieController funcs

---
+++ api/controllers/smoothieControllers.js
@@ -0,0 +1,21 @@
+
+
+export const getAllSmoothies = (req,res) => {
+
+}
+
+export const getAllUserSmoothies = (req,res) => {
+
+}
+
+export const getSingleSmoothie = (req,res) => {
+
+}
+
+export const addSmoothie = (req,res) => {
+
+}
+
+export const deleteSmoothie = (req,res) => {
+
+}
