7037f90acb5450b48811016211c48bdd08bd1582
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-03 18:04:45 +0530

    error handling messages

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,22 +1,24 @@
 import mongoose from "mongoose";
+import { isEmail } from 'validator'
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
-        unique: true,
-        lowercase: true
+        required: [true, 'Email is required'],
+        unique: [true, 'Email should be unique'],
+        lowercase: [true, 'Email should be in lowercase'],
+        validate: [isEmail, 'Not a valid Email']
     },
     username: {
         type: String,
-        required: true,
-        unique: true,
-        lowercase: true
+        required: [true, 'Username is required'],
+        unique: [true, 'Username should be unique'],
+        lowercase: [true, 'Username should be in lowercase']
     },
     password: {
         type: String,
-        required: true,
-        minLength: 8
+        required: [true, 'Password is required'],
+        minlength: [8, 'Minimum password length is 8']
     }
 })
 
