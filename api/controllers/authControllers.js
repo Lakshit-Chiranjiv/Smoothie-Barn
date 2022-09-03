e936ad5547589ff80fff55d571267c89751ea026
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-03 17:09:26 +0530

    authController funcs

---
+++ api/controllers/authControllers.js
@@ -0,0 +1,21 @@
+
+
+export const getSignupPage = (req,res) => {
+
+}
+
+export const getLoginPage = (req,res) => {
+
+}
+
+export const signupUser = (req,res) => {
+
+}
+
+export const loginUser = (req,res) => {
+
+}
+
+export const logoutUser = (req,res) => {
+
+}
