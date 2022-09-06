440eceee9fe49a2af20e748c3be0e932db47f490
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-06 17:55:03 +0530

    wired smoothie controllers

--- api/routes/smoothieRoutes.js
+++ api/routes/smoothieRoutes.js
@@ -1,20 +1,21 @@
 import express from 'express'
+import { addSmoothie, deleteSmoothie, getAllSmoothies, getAllUserSmoothies, getSingleSmoothie } from '../controllers/smoothieControllers'
 
 const router = express.Router()
 
 //get all smoothies
-router.get('/smoothies',()=>{})
+router.get('/smoothies',getAllSmoothies)
 
 //get all smoothies for respective user by id
-router.get('/:userid/smoothies',()=>{})
+router.get('/:userid/smoothies',getAllUserSmoothies)
 
 //add new smoothie
-router.post('/addsmoothie',()=>{})
+router.post('/addsmoothie',addSmoothie)
 
 //get single smoothie data
-router.get('/smoothie/:id',()=>{})
+router.get('/smoothie/:id',getSingleSmoothie)
 
 //delete smoothie
-router.delete('/delete/:id',()=>{})
+router.delete('/delete/:id',deleteSmoothie)
 
 export default router
