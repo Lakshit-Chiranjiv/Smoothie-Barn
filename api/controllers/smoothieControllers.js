95eff0d93b0996a33dd8757f4dea50fee0a499bd
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-10 13:47:56 +0530

    get all smoothies func

--- api/controllers/smoothieControllers.js
+++ api/controllers/smoothieControllers.js
@@ -1,7 +1,19 @@
+import SmoothieModel from "../models/smoothieModel.js"
+
 //also send res.locals.user from all the get requests
 
-export const getAllSmoothies = (req,res) => {
-
+export const getAllSmoothies = async(req,res) => {
+    try {
+        const smoothies = await SmoothieModel.find().sort({ createdAt: -1 })
+        const user = res.locals.user
+        const sendRes = {
+            smoothies: smoothies,
+            user: user
+        }
+        res.status(200).json(sendRes)
+    } catch (error) {
+        console.log("Could not fetch all smoothies",error)
+    }
 }
 
 export const getAllUserSmoothies = (req,res) => {
