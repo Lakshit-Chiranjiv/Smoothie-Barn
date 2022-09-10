d5c9564810474b1928f8ba8e2a3796ff25f6033f
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-10 11:27:08 +0530

    user check middleware

---
+++ api/middleware/userMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+import UserModel from '../models/userModel'
+
+const userCheck = (req,res,next) => {
+    const token = req.cookies.jwt
+
+    if(token){
+        jwt.verify(token,'lakshit secret key',async(err,decodedToken)=>{
+            if(err){
+                res.locals.user = null
+                next()
+            }
+            else{
+                const user = UserModel.findById(decodedToken.id)
+                res.locals.user = user
+                next()
+            }
+        })
+    }
+    else{
+        res.locals.user = null
+        next()
+    }
+}
+
+export default userCheck
