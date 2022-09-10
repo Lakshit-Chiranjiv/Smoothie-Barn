3ac2cbe7b7647443cdeefe9d9cb3c9a5abffc16e
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-10 10:28:13 +0530

    login user func with error handling for login

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -11,6 +11,15 @@ const handleErrors = (error) => {
 
     const { message,code } = error
 
+    //login errors
+    if(message === 'Unregistered Email')
+        errors.email = message
+    if(message === 'Incorrect Username')
+        errors.username = message
+    if(message === 'Wrong Password')
+        errors.password = message
+
+
     if(code === 11000){
         if(error.keyPattern.email){
             errors.email = 'Already registered email'
@@ -64,8 +73,18 @@ export const signupUser = async(req,res) => {
     }
 }
 
-export const loginUser = (req,res) => {
+export const loginUser = async(req,res) => {
+    const { email, username, password } = req.body
 
+    try {
+        const user = await UserModel.login(email,username,password)
+        const token = createToken(user._id)
+        res.cookie('jwt',token,{ httpOnly: true, maxAge: maxTokenAge * 1000 })
+        res.status(200).json({ user: user._id })
+    } catch (error) {
+        const errorsObj = handleErrors(error)
+        res.status(400).json(errorsObj)
+    }
 }
 
 export const logoutUser = (req,res) => {
