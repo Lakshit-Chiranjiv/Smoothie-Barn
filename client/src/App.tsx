80eca68306ee3836ce66021a5cdad1f6dc46511f
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-13 23:49:50 +0530

    layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
+import { Container,Grid,AppShell,Navbar,Header } from '@mantine/core'
 
 function App() {
 
   return (
-    <div className="App">
-
-    </div>
+    <AppShell
+      padding="md"
+      navbar={<Navbar width={{ base: 300 }} height={500} p="xs">{/* Navbar content */}</Navbar>}
+      header={<Header height={60} p="xs">{/* Header content */}</Header>}
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+    >
+      <Container>
+        {/* <Grid>
+          
+        </Grid> */}
+        <h1>Hello Site</h1>
+      </Container>
+    </AppShell>
   )
 }
 
