ab53d0a5ec8df75a7219a4a7665637321547c472
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-14 23:40:55 +0530

    margin 0

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Container, Title } from '@mantine/core'
 
 const Footer = () => {
   return (
-    <Container className='footer' p={16}>
+    <Container className='footer' p={16} m={0}>
         <Title order={5} color='white'>
             © Copyright | {new Date().getFullYear()} | Created By Lakshit | All rights reserved
         </Title>
