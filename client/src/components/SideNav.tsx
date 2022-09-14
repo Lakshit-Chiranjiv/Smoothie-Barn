f63e6e3125c53f4d2ce6aefea6a623b8da7fc197
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-14 17:27:32 +0530

    file for sidenav

---
+++ client/src/components/SideNav.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@mantine/core'
+import React from 'react'
+
+const SideNav = () => {
+  return (
+    <Container className='sidenav'>
+        
+    </Container>
+  )
+}
+
+export default SideNav
