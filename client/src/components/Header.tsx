691e8ca267d976eb9d5a9867684eef97d335ef0a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-14 18:11:55 +0530

    Header Component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Container,Image } from '@mantine/core'
+import logo from './../assets/logo1.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Container className='header'>
+      <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+        <Image
+          radius="md"
+          src={logo}
+          alt="Logo Image"
+        />
+      </div>
+    </Container>
   )
 }
 
