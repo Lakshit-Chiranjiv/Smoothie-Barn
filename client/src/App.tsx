6e37e30e2f82ba7ffb09218b85522590b93f8454
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-14 23:40:39 +0530

    added mainarea

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,25 +4,27 @@ import { Grid, Group } from '@mantine/core';
 import HeroPage from './components/HeroPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import SideNav from './components/SideNav';
+import MainArea from './components/MainArea';
 
 export default function App() {
   return (
     <>
       <Grid grow>
-        <Grid.Col>
+        <Grid.Col span={12}>
             <Header/>
         </Grid.Col>
       </Grid>
       <Grid grow>
-        <Grid.Col span={4}>
-          Side Nav
+        <Grid.Col span={3}>
+          <SideNav/>
         </Grid.Col>
-        <Grid.Col span={8}>
-          Main Area
+        <Grid.Col span={9}>
+          <MainArea/>
         </Grid.Col>
       </Grid>
       <Grid grow>
-        <Grid.Col>
+        <Grid.Col span={12}>
           <Footer/>
         </Grid.Col>
       </Grid>
