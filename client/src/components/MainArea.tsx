69827f1616330d4704165a431a3e564460fb9408
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-14 17:29:07 +0530

    main area file

---
+++ client/src/components/MainArea.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@mantine/core'
+import React from 'react'
+
+const MainArea = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default MainArea
