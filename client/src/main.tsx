26102638ab89ddbe8d5568aab494e11e66662a9c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-18 00:07:46 +0530

    theme fontfamily change

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
+import { MantineProvider } from '@mantine/core'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { CustomFonts } from './lib/CustomFonts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={{
+        fontFamily: 'Poppins, sans-serif',
+    }}>
+      <CustomFonts/>
+      <App/>
+    </MantineProvider>
   </React.StrictMode>
 )
