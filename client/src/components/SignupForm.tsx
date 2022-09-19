48deeb80f3b69420446afde03194df7117eebf57
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-20 00:05:30 +0530

    sign up form elements

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
+import { Container, PasswordInput, TextInput } from '@mantine/core'
 const SignupForm = () => {
   return (
-    <div>SignupForm</div>
+    <Container className='signupform'>
+        <TextInput
+            placeholder="Your username"
+            label="Username"
+            withAsterisk
+            mb={40}
+        />
+        <TextInput
+            placeholder="Your email"
+            label="Email"
+            withAsterisk
+            mb={40}
+        />
+        <PasswordInput
+            placeholder="Password"
+            label="Password"
+            description="Password must include at least one letter, number and special character"
+            withAsterisk
+            mb={40}
+        />
+    </Container>
   )
 }
 
