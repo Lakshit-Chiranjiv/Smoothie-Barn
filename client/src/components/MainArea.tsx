1a9e0ccbc757dc0024d4e37754ceaf11400e9545
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-20 22:42:23 +0530

    added authtabs

--- client/src/components/MainArea.tsx
+++ client/src/components/MainArea.tsx
@@ -1,11 +1,13 @@
 import { Container } from '@mantine/core'
 import React from 'react'
+import AuthTabs from './AuthTabs'
 import HeroPage from './HeroPage'
 
 const MainArea = () => {
   return (
     <Container className='mainarea'>
         {/* <HeroPage/> */}
+        <AuthTabs/>
     </Container>
   )
 }
