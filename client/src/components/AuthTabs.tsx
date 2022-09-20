7ffc425e68e1fe3289c9568948907311467be41c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-20 22:42:02 +0530

    auth tabs component

---
+++ client/src/components/AuthTabs.tsx
@@ -0,0 +1,24 @@
+import { Tabs } from '@mantine/core';
+import { IconUserPlus,IconUserCircle } from '@tabler/icons';
+import AuthForm from './AuthForm';
+
+function AuthTabs() {
+  return (
+    <Tabs variant='pills' defaultValue="signup" py={20}>
+      <Tabs.List px={40}>
+        <Tabs.Tab value="signup" icon={<IconUserPlus size={14} />}>Sign Up</Tabs.Tab>
+        <Tabs.Tab value="login" icon={<IconUserCircle size={14} />}>Login</Tabs.Tab>
+      </Tabs.List>
+
+      <Tabs.Panel value="signup" pt="xs">
+        <AuthForm formFor='signup'/>
+      </Tabs.Panel>
+
+      <Tabs.Panel value="login" pt="xs">
+        <AuthForm formFor='login'/>
+      </Tabs.Panel>
+    </Tabs>
+  );
+}
+
+export default AuthTabs;
