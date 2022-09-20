023f3020097f0558363fab44e7db9c1926dae45b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-20 22:03:24 +0530

    added signupform

--- client/src/components/MainArea.tsx
+++ client/src/components/MainArea.tsx
@@ -1,11 +1,13 @@
 import { Container } from '@mantine/core'
 import React from 'react'
 import HeroPage from './HeroPage'
+import SignupForm from './SignupForm'
 
 const MainArea = () => {
   return (
     <Container className='mainarea'>
-        <HeroPage/>
+        {/* <HeroPage/> */}
+        <SignupForm/>
     </Container>
   )
 }
