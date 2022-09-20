d3c74d8a2bda45d6dbb2b67571c41a11e200fb83
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-20 22:29:19 +0530

    removed signup form

--- client/src/components/MainArea.tsx
+++ client/src/components/MainArea.tsx
@@ -1,13 +1,11 @@
 import { Container } from '@mantine/core'
 import React from 'react'
 import HeroPage from './HeroPage'
-import SignupForm from './SignupForm'
 
 const MainArea = () => {
   return (
     <Container className='mainarea'>
         {/* <HeroPage/> */}
-        <SignupForm/>
     </Container>
   )
 }
