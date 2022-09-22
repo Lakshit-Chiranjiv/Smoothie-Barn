afdaaf5709519ff2710065d10f68316fcbb9e478
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-22 23:47:03 +0530

    centered footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Container, Title } from '@mantine/core'
 const Footer = () => {
   return (
     <Container className='footer' p={16} m={0}>
-        <Title order={5} color='white' sx={{ fontFamily: 'Poppins, sans-serif' }}>
+        <Title order={5} color='white' sx={{ fontFamily: 'Poppins, sans-serif',textAlign: 'center' }}>
             © Copyright | {new Date().getFullYear()} | Created By Lakshit | All rights reserved
         </Title>
     </Container>
