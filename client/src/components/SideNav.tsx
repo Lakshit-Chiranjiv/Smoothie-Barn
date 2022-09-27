33edd07ad7beb1c832dd94cfcc2e4f0eb9eb8a16
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-27 14:25:52 +0530

    added all the nav links

--- client/src/components/SideNav.tsx
+++ client/src/components/SideNav.tsx
@@ -1,12 +1,19 @@
 import { Avatar, Container, Group, Title } from '@mantine/core'
+import { IconCirclePlus, IconGlass, IconHome, IconLogin, IconLogout, IconUserPlus } from '@tabler/icons'
 import NavbarLink from './NavbarLink'
 
 const SideNav = () => {
   return (
     <Container className='sidenav' py={40} style={{ position: 'relative' }}>
-      {
-        [1,2,3,4].map((val,i) => <NavbarLink key={i}/>)
-      }
+
+      <Container p={0}>
+        <NavbarLink linkLabel='Home' linkIcon={<IconHome/>}/>
+        <NavbarLink linkLabel='Smoothies' linkIcon={<IconGlass/>}/>
+        <NavbarLink linkLabel='Add Smoothie' linkIcon={<IconCirclePlus/>}/>
+        <NavbarLink linkLabel='Sign Up' linkIcon={<IconUserPlus/>}/>
+        <NavbarLink linkLabel='Log in' linkIcon={<IconLogin/>}/>
+        <NavbarLink linkLabel='Log out' linkIcon={<IconLogout/>}/>
+      </Container>
 
       <Group position='apart' color='blue' style={{ width: '90%', position: 'absolute', bottom: 30, backgroundColor: '#f8ceff', borderRadius: 10 }} p={12}>
         <Avatar src={null} alt="Vitaly Rtishchev" color="violet">VR</Avatar>
