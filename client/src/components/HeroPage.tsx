db327ef1570b6fe2923ca991ecaf88669b621008
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-28 16:59:05 +0530

    added signed in state buttons for hero page

--- client/src/components/HeroPage.tsx
+++ client/src/components/HeroPage.tsx
@@ -14,12 +14,23 @@ const HeroPage = () => {
       </div>
       <Title order={1} sx={{ fontFamily: 'Poppins, sans-serif',textAlign: 'center' }} my={20}>Welcome to The Smoothie Barn</Title>
 
+      {/* Not signed in */}
       <Group position='center' my={40}>
         <div style={{ width: '200px' }}>
           <Button variant="gradient" fullWidth gradient={{ from: '#e89dff', to: '#a49dff', deg: 35 }}>Sign Up</Button>
         </div>
         <div style={{ width: '200px' }}>
           <Button variant="gradient" fullWidth gradient={{ from: '#b4ff9d', to: '#ffb983', deg: 35 }}>Login</Button>
+        </div>
+      </Group>
+
+      {/* signed in */}
+      <Group position='center' my={40}>
+        <div style={{ width: '200px' }}>
+          <Button variant="gradient" fullWidth gradient={{ from: '#e89dff', to: '#a49dff', deg: 35 }}>Smoothies</Button>
+        </div>
+        <div style={{ width: '200px' }}>
+          <Button variant="gradient" fullWidth gradient={{ from: '#b4ff9d', to: '#ffb983', deg: 35 }}>About Me</Button>
         </div>
       </Group>
     </Container>
