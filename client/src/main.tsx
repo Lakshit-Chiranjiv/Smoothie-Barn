34226f99f3f5236d2e8280f893ffcbbc7467edf9
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-30 23:31:28 +0530

    added browser router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,20 @@
 import { MantineProvider } from '@mantine/core'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { CustomFonts } from './lib/CustomFonts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MantineProvider theme={{
-        fontFamily: 'Poppins, sans-serif',
-    }}>
-      <CustomFonts/>
-      <App/>
-    </MantineProvider>
+    <BrowserRouter>
+      <MantineProvider theme={{
+          fontFamily: 'Poppins, sans-serif',
+      }}>
+        <CustomFonts/>
+        <App/>
+      </MantineProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
