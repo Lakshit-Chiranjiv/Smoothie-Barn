96e7a21e2ac3441059db8ee73fb4da6fdc3e2ba0
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-30 23:39:42 +0530

    implemented routes

--- client/src/components/MainArea.tsx
+++ client/src/components/MainArea.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@mantine/core'
 import React, { useState } from 'react'
+import { Route,Routes } from 'react-router-dom'
 import AboutPage from './AboutPage'
 import AddSmoothie from './AddSmoothie'
 import AuthTabs from './AuthTabs'
@@ -8,18 +9,22 @@ import NotFound from './NotFound'
 import SmoothieDetail from './SmoothieDetail'
 import SmoothiesList from './SmoothiesList'
 
+
 const MainArea = () => {
 
   const [addModalOpened,setAddModalOpened] = useState(false)
 
   return (
     <Container className='mainarea'>
-        {/* <HeroPage/> */}
-        {/* <AuthTabs/> */}
-        {/* <SmoothiesList/> */}
-        {/* <SmoothieDetail/> */}
-        {/* <AboutPage/> */}
-        <NotFound/>
+      <Routes>
+        <Route path='/' element={<HeroPage/>}/>
+        <Route path='/signup' element={<AuthTabs/>}/>
+        <Route path='/login' element={<AuthTabs/>}/>
+        <Route path='/smoothies' element={<SmoothiesList/>}/>
+        <Route path='/smoothies/:id' element={<SmoothieDetail/>}/>
+        <Route path='/about' element={<AboutPage/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
         <AddSmoothie addModalOpened={addModalOpened} setAddModalOpened={setAddModalOpened}/>
     </Container>
   )
