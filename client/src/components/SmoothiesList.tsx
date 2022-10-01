f0dee4d03bfd09fa55608e8c0ecee0a9f7129824
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-01 19:32:24 +0530

    added open add modal func on btn click in smoothie list component

--- client/src/components/SmoothiesList.tsx
+++ client/src/components/SmoothiesList.tsx
@@ -1,12 +1,16 @@
 import { Button, Divider, Grid, Group, Title } from '@mantine/core'
 import SmoothieCard from './SmoothieCard'
 
-const SmoothiesList = () => {
+type SmoothieListProps = {
+  setAddModalOpened: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const SmoothiesList = ({setAddModalOpened}: SmoothieListProps) => {
   return (
     <>
       <Group position='apart' m={30}>
         <Title order={2} sx={{ fontFamily: 'Poppins, sans-serif' }}>The Barn</Title>
-        <Button variant="gradient" gradient={{ from: 'teal', to: 'blue', deg: 60 }}>Add Smoothie</Button>
+        <Button variant="gradient" gradient={{ from: 'teal', to: 'blue', deg: 60 }} onClick={()=>setAddModalOpened(true)}>Add Smoothie</Button>
       </Group>
 
       <Divider my="sm" />
