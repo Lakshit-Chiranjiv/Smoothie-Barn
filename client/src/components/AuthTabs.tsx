0fd64e12df0b015163c04ec8e1e55390bac88097
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-01 23:26:28 +0530

    auth tab state wiring and setters

--- client/src/components/AuthTabs.tsx
+++ client/src/components/AuthTabs.tsx
@@ -2,12 +2,17 @@ import { Tabs } from '@mantine/core';
 import { IconUserPlus,IconUserCircle } from '@tabler/icons';
 import AuthForm from './AuthForm';
 
-function AuthTabs() {
+type AuthTabsProps = {
+  authTab: 'signup' | 'login',
+  setAuthTab: React.Dispatch<React.SetStateAction<"signup" | "login">>
+}
+
+function AuthTabs({authTab,setAuthTab}: AuthTabsProps) {
   return (
-    <Tabs variant='pills' defaultValue="signup" py={20} color='indigo.4'>
+    <Tabs variant='pills' defaultValue='signup' value={authTab} py={20} color='indigo.4'>
       <Tabs.List px={40} position='center'>
-        <Tabs.Tab value="signup" icon={<IconUserPlus size={16} />}>Sign Up</Tabs.Tab>
-        <Tabs.Tab value="login" icon={<IconUserCircle size={22} />}>Login</Tabs.Tab>
+        <Tabs.Tab value="signup" icon={<IconUserPlus size={16} />} onClick={()=>setAuthTab('signup')}>Sign Up</Tabs.Tab>
+        <Tabs.Tab value="login" icon={<IconUserCircle size={22} />} onClick={()=>setAuthTab('login')}>Login</Tabs.Tab>
       </Tabs.List>
 
       <Tabs.Panel value="signup" pt="xs">
