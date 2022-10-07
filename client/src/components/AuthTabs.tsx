5ed64ffe21af4478bad07607b5abc23360df17db
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-07 09:42:20 +0530

    passed setuser to authForm and defined its types

--- client/src/components/AuthTabs.tsx
+++ client/src/components/AuthTabs.tsx
@@ -1,13 +1,15 @@
 import { Tabs } from '@mantine/core';
 import { IconUserPlus,IconUserCircle } from '@tabler/icons';
+import { userStateType } from '../lib/Types.js';
 import AuthForm from './AuthForm';
 
 type AuthTabsProps = {
   authTab: 'signup' | 'login',
-  setAuthTab: React.Dispatch<React.SetStateAction<"signup" | "login">>
+  setAuthTab: React.Dispatch<React.SetStateAction<"signup" | "login">>,
+  setUser: React.Dispatch<React.SetStateAction<userStateType | null>>
 }
 
-function AuthTabs({authTab,setAuthTab}: AuthTabsProps) {
+function AuthTabs({authTab,setAuthTab,setUser}: AuthTabsProps) {
   return (
     <Tabs variant='pills' defaultValue='signup' value={authTab} py={20} color='indigo.4'>
       <Tabs.List px={40} position='center'>
@@ -16,11 +18,11 @@ function AuthTabs({authTab,setAuthTab}: AuthTabsProps) {
       </Tabs.List>
 
       <Tabs.Panel value="signup" pt="xs">
-        <AuthForm formFor='signup'/>
+        <AuthForm formFor='signup' setUser={setUser}/>
       </Tabs.Panel>
 
       <Tabs.Panel value="login" pt="xs">
-        <AuthForm formFor='login'/>
+        <AuthForm formFor='login' setUser={setUser}/>
       </Tabs.Panel>
     </Tabs>
   );
