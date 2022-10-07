36934c17f83fdaed9a5dfad4be21c87c537eaf92
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-07 09:41:58 +0530

    passed setUSer to authtabs and defined its types

--- client/src/components/MainArea.tsx
+++ client/src/components/MainArea.tsx
@@ -11,22 +11,23 @@ import { userStateType } from './../lib/Types.js'
 
 type MainAreaProps = {
   user: userStateType | null,
+  setUser: React.Dispatch<React.SetStateAction<userStateType | null>>,
   addModalOpened: boolean,
   setAddModalOpened: React.Dispatch<React.SetStateAction<boolean>>,
   authTab: 'signup' | 'login',
   setAuthTab: React.Dispatch<React.SetStateAction<"signup" | "login">>
 }
 
-const MainArea = ({user,addModalOpened,setAddModalOpened,authTab,setAuthTab}: MainAreaProps) => {
+const MainArea = ({user,setUser,addModalOpened,setAddModalOpened,authTab,setAuthTab}: MainAreaProps) => {
 
   return (
     <Container className='mainarea'>
       <Routes>
         <Route path='*' element={<NotFound/>}/>
         <Route path='/' element={<HeroPage/>}/>
-        <Route path='/signup' element={!user ? <AuthTabs authTab={authTab} setAuthTab={setAuthTab}/> : <Navigate to='/'/>}/>
-        <Route path='/login' element={!user ? <AuthTabs authTab={authTab} setAuthTab={setAuthTab}/> : <Navigate to='/'/>}/>
-        <Route path='/smoothies' element={user ? <SmoothiesList setAddModalOpened={setAddModalOpened}/> : <Navigate to='/'/>}/>
+        <Route path='/signup' element={!user ? <AuthTabs authTab={authTab} setAuthTab={setAuthTab} setUser={setUser}/> : <Navigate to='/'/>}/>
+        <Route path='/login' element={!user ? <AuthTabs authTab={authTab} setAuthTab={setAuthTab} setUser={setUser}/> : <Navigate to='/'/>}/>
+        <Route path='/smoothies' element={user ? <SmoothiesList user={user} setAddModalOpened={setAddModalOpened}/> : <Navigate to='/'/>}/>
         <Route path='/smoothies/:id' element={user ? <SmoothieDetail/> : <Navigate to='/'/>}/>
         <Route path='/about' element={<AboutPage/>}/>
       </Routes>
