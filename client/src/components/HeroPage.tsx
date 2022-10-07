75b4fd5777a7f0abb9f1c8dfa8d07df62ba6004d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-07 10:41:28 +0530

    conditionally rendered buttons based on user

--- client/src/components/HeroPage.tsx
+++ client/src/components/HeroPage.tsx
@@ -1,8 +1,13 @@
 import { Button, Container,Group,Image,Title } from '@mantine/core'
 import { Link } from 'react-router-dom'
+import { userStateType } from '../lib/Types.js'
 import heroImg1 from './../assets/hero.png'
 
-const HeroPage = () => {
+type HeroPageProps = {
+  user: userStateType | null
+}
+
+const HeroPage = ({ user }: HeroPageProps) => {
   return (
     <Container className='hero' fluid>
       <div style={{ width: 360, marginLeft: 'auto', marginRight: 'auto' }}>
@@ -16,32 +21,38 @@ const HeroPage = () => {
       <Title order={1} sx={{ fontFamily: 'Poppins, sans-serif',textAlign: 'center' }} my={20}>Welcome to The Smoothie Barn</Title>
 
       {/* Not signed in */}
-      <Group position='center' my={40}>
-        <div style={{ width: '200px' }}>
-          <Link to='/signup' className='link'>
-            <Button variant="gradient" fullWidth gradient={{ from: '#e89dff', to: '#a49dff', deg: 35 }}>Sign Up</Button>
-          </Link>
-        </div>
-        <div style={{ width: '200px' }}>
-          <Link to='/login' className='link'>
-            <Button variant="gradient" fullWidth gradient={{ from: '#b4ff9d', to: '#ffb983', deg: 35 }}>Login</Button>
-          </Link>
-        </div>
-      </Group>
+      {
+        !user && 
+        <Group position='center' my={40}>
+          <div style={{ width: '200px' }}>
+            <Link to='/signup' className='link'>
+              <Button variant="gradient" fullWidth gradient={{ from: '#e89dff', to: '#a49dff', deg: 35 }}>Sign Up</Button>
+            </Link>
+          </div>
+          <div style={{ width: '200px' }}>
+            <Link to='/login' className='link'>
+              <Button variant="gradient" fullWidth gradient={{ from: '#b4ff9d', to: '#ffb983', deg: 35 }}>Login</Button>
+            </Link>
+          </div>
+        </Group>
+      }
 
       {/* signed in */}
-      <Group position='center' my={40}>
-        <div style={{ width: '200px' }}>
-          <Link to='/smoothies' className='link'>
-            <Button variant="gradient" fullWidth gradient={{ from: '#e89dff', to: '#a49dff', deg: 35 }}>Smoothies</Button>
-          </Link>
-        </div>
-        <div style={{ width: '200px' }}>
-          <Link to='/about' className='link'>
-            <Button variant="gradient" fullWidth gradient={{ from: '#b4ff9d', to: '#ffb983', deg: 35 }}>About Me</Button>
-          </Link>
-        </div>
-      </Group>
+      {
+        user && 
+        <Group position='center' my={40}>
+          <div style={{ width: '200px' }}>
+            <Link to='/smoothies' className='link'>
+              <Button variant="gradient" fullWidth gradient={{ from: '#e89dff', to: '#a49dff', deg: 35 }}>Smoothies</Button>
+            </Link>
+          </div>
+          <div style={{ width: '200px' }}>
+            <Link to='/about' className='link'>
+              <Button variant="gradient" fullWidth gradient={{ from: '#b4ff9d', to: '#ffb983', deg: 35 }}>About Me</Button>
+            </Link>
+          </div>
+        </Group>
+      }
     </Container>
   )
 }
