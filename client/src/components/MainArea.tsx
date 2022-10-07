dae594aae35bb8410eeca7c72d91147b37ae8a21
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-07 09:13:52 +0530

    conditionally rendered all routes and redirects

--- client/src/components/MainArea.tsx
+++ client/src/components/MainArea.tsx
@@ -1,5 +1,5 @@
 import { Container } from '@mantine/core'
-import { Route,Routes } from 'react-router-dom'
+import { Route,Routes,Navigate } from 'react-router-dom'
 import AboutPage from './AboutPage'
 import AddSmoothie from './AddSmoothie'
 import AuthTabs from './AuthTabs'
@@ -17,20 +17,23 @@ type MainAreaProps = {
   setAuthTab: React.Dispatch<React.SetStateAction<"signup" | "login">>
 }
 
-const MainArea = ({addModalOpened,setAddModalOpened,authTab,setAuthTab}: MainAreaProps) => {
+const MainArea = ({user,addModalOpened,setAddModalOpened,authTab,setAuthTab}: MainAreaProps) => {
 
   return (
     <Container className='mainarea'>
       <Routes>
         <Route path='*' element={<NotFound/>}/>
         <Route path='/' element={<HeroPage/>}/>
-        <Route path='/signup' element={<AuthTabs authTab={authTab} setAuthTab={setAuthTab}/>}/>
-        <Route path='/login' element={<AuthTabs authTab={authTab} setAuthTab={setAuthTab}/>}/>
-        <Route path='/smoothies' element={<SmoothiesList setAddModalOpened={setAddModalOpened}/>}/>
-        <Route path='/smoothies/:id' element={<SmoothieDetail/>}/>
+        <Route path='/signup' element={!user ? <AuthTabs authTab={authTab} setAuthTab={setAuthTab}/> : <Navigate to='/'/>}/>
+        <Route path='/login' element={!user ? <AuthTabs authTab={authTab} setAuthTab={setAuthTab}/> : <Navigate to='/'/>}/>
+        <Route path='/smoothies' element={user ? <SmoothiesList setAddModalOpened={setAddModalOpened}/> : <Navigate to='/'/>}/>
+        <Route path='/smoothies/:id' element={user ? <SmoothieDetail/> : <Navigate to='/'/>}/>
         <Route path='/about' element={<AboutPage/>}/>
       </Routes>
+      {
+        user &&
         <AddSmoothie addModalOpened={addModalOpened} setAddModalOpened={setAddModalOpened}/>
+      }
     </Container>
   )
 }
