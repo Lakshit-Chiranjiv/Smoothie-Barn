b8eff3bd40387429a51cca258605ba2ba72e2956
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-13 15:16:21 +0530

    input states and onchange handlers with 2 way bind

--- client/src/components/AddSmoothie.tsx
+++ client/src/components/AddSmoothie.tsx
@@ -1,5 +1,5 @@
 import { Button, Modal, NumberInput, Textarea, TextInput, Title } from '@mantine/core'
-import React from 'react'
+import React, { useState } from 'react'
 
 type AddSmoothieProps = {
     addModalOpened: boolean,
@@ -7,6 +7,10 @@ type AddSmoothieProps = {
 }
 
 const AddSmoothie = ({addModalOpened,setAddModalOpened}: AddSmoothieProps) => {
+
+  const [smoothieName,setSmoothieName] = useState('')
+  const [smoothiePrice,setSmoothiePrice] = useState(0)
+  const [smoothieSteps,setSmoothieSteps] = useState<string[]>([])
   return (
     <Modal withCloseButton={false} opened={addModalOpened} onClose={() => setAddModalOpened(false)}>
       <Title order={2} mb={30}>Add a Smoothie</Title>
@@ -15,6 +19,8 @@ const AddSmoothie = ({addModalOpened,setAddModalOpened}: AddSmoothieProps) => {
         label="Smoothie Name"
         withAsterisk
         mb={20}
+        value={smoothieName}
+        onChange={(e) => setSmoothieName(e.target.value)}
       />
       <NumberInput
         defaultValue={0}
@@ -23,11 +29,15 @@ const AddSmoothie = ({addModalOpened,setAddModalOpened}: AddSmoothieProps) => {
         label="Smoothie Price in ₹"
         withAsterisk
         mb={20}
+        value={smoothiePrice}
+        onChange={(num)=>setSmoothiePrice(num?num:0)}
       />
       <Textarea
         placeholder="Put a full stop after each step"
         label="Steps"
         mb={20}
+        value={smoothieSteps.join()}
+        onChange={(e)=>setSmoothieSteps(e.target.value.split('.'))}
       />
       <Button fullWidth variant="gradient" gradient={{ from: 'teal', to: 'blue', deg: 60 }}>Add Smoothie</Button>
     </Modal>
